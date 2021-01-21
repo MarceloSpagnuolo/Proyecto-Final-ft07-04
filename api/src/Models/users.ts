cd59d1e48722ef99bdfe96a188c02298cbc44855
babinobass <69868198+babinobass@users.noreply.github.com>
2021-01-21 13:40:59 -0300

    primeros intentos

--- api/src/Models/users.ts
+++ api/src/Models/users.ts
@@ -1,8 +1,24 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose, { Document, Schema } from "mongoose";
+import * as bcrypt from "bcrypt";
 
-var UserSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+interface Props extends Document {
+    _id: any
+    name: any;
+    facebookId: string,
+    googleId: string,
+    thumbnail: Buffer,
+    role: string,
+    email: string,
+    password: string,
+    created: any
+    cohorte: any,
+    standup: any,
+    pairprograming: any,
+}
+
+
+const UserSchema: Schema<Props> = new Schema({
+    _id: Schema.Types.ObjectId,
     name: {
         firsname: { type: String, required: true },
         lastname: { type: String, required: true }
@@ -16,18 +32,19 @@ var UserSchema = new mongoose.Schema({
     created: { type: Date, default: Date.now },
     cohorte: { type: mongoose.Schema.Types.ObjectId, ref: "Cohortes" },
     standup: { type: mongoose.Schema.Types.ObjectId, ref: "Standups" },
-    pairprograming: [ { type: mongoose.Schema.Types.ObjectId, ref: "PairProgramins" } ],
+    pairprograming: [{ type: mongoose.Schema.Types.ObjectId, ref: "PairProgramins" }],
 });
 
-UserSchema.pre("save", function(next) {
+UserSchema.pre("save", function (next) {
     if (!this.isModified("password")) return next();
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(this.password, salt);
     this.password = hash;
 });
 
-UserSchema.methods.compare = function(candidatePassword) {
-    return bcrypt.compareSync(candidatePassword, this.password);
-};
+UserSchema.method('comparePassword', function (password: string): boolean {
+    if (bcrypt.compareSync(password, this.password)) return true;
+    return false;
+});
 
 module.exports = mongoose.model("Users", UserSchema);
