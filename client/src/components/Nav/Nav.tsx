48b39a7df08f73dfe6b9de9a735eeab483f0884f
Marcelo Spagnuolo <lu4huf@gmail.com>
2021-01-22 00:59:58 -0300

    inicion navigation

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Nav.css";
 
-
-const Nav = () => {
+const Navigation = () => {
     return (
-        <h1>Holi</h1>
+        <div className="Navigation-Container">
+            <img className="Navigation-Logo" src="https://media.licdn.com/dms/image/C4E0BAQE2nmZshIwV9A/company-logo_200_200/0?e=2159024400&v=beta&t=2rZ7n-f_mLfkkqAxz0B8IVGTELeBH1VTHBm0naezmZw" />
+            <div>
+                <h2 className="Navigation-Henry">Henry</h2>
+                <h2 className="Navigation-Manager">Manager</h2>
+            </div>
+        </div>
     )
 }
 
-export default Nav;
+export default Navigation;
