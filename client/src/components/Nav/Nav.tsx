ba13b12c85814e477e359638bfcf049377930a48
MarceloSpagnuolo <71037386+MarceloSpagnuolo@users.noreply.github.com>
2021-01-22 14:08:02 -0300

    navigation con bootstrap

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Nav.css";
 
 const Navigation = () => {
-    return (
-        <div className="Navigation-Container">
-            <img className="Navigation-Logo" src="https://media.licdn.com/dms/image/C4E0BAQE2nmZshIwV9A/company-logo_200_200/0?e=2159024400&v=beta&t=2rZ7n-f_mLfkkqAxz0B8IVGTELeBH1VTHBm0naezmZw" />
-            <div>
-                <h2 className="Navigation-Henry">Henry</h2>
-                <h2 className="Navigation-Manager">Manager</h2>
-            </div>
-        </div>
-    )
-}
+  return (
+    <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Henry Manager</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavDropdown title="Pepito" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#">Mi perfil</NavDropdown.Item>
+            <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Alumnos" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#">Invitaciones</NavDropdown.Item>
+            <NavDropdown.Item href="#">Gestión de Alumnos</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Cohortes" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#">Gestión de Cohortes</NavDropdown.Item>
+            <NavDropdown.Item href="#">Gestión de Grupos</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Navigation;
