c63bab58eff601e5373c08c54ebcaf91863184df
andresogc <andresogc2011@gmail.com>
2021-01-23 14:44:16 -0500

    Crear ruta post para registrar un nuevo  usuario  y delete para eliminar un usuario por id

---
+++ api/src/Models/users.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document, Schema } from "mongoose";
+import * as bcrypt from "bcrypt";
+
+interface Props extends Document {
+    name: any,
+    facebookId?: string,
+    googleId?: string,
+    thumbnail?: Buffer,
+    role: string,
+    email: string,
+    password: string,
+    created: any,
+    cohorte?: any,
+    standup?: any,
+    pairprograming?: any,
+}
+
+
+const UserSchema: Schema<Props> = new Schema({
+    name: {
+        firstname: { type: String, required: true,trim:true },
+        lastname: { type: String, required: true,trim:true }
+    },
+    facebookId: String,
+    googleId: String,
+    thumbnail: Buffer,
+    role: String,
+    email: {type:String,trim:true,unique:true},
+    password: {type:String,trim:true},
+    created: { type: Date, default: Date.now },
+    cohorte: { type: mongoose.Schema.Types.ObjectId, ref: "Cohortes" },
+    standup: { type: mongoose.Schema.Types.ObjectId, ref: "Standups" },
+    pairprograming: [{ type: mongoose.Schema.Types.ObjectId, ref: "PairProgramins" }], 
+});
+
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSaltSync(10);
+    const hash = await bcrypt.hashSync(this.password, salt);
+    this.password = hash;
+    
+});
+
+UserSchema.method('comparePassword', function (password: string): boolean {
+    if (bcrypt.compareSync(password, this.password)) return true;
+    return false;
+});
+
+export default mongoose.model("Users", UserSchema);
