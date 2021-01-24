24bdd1bca56f3fcb12c7a0cd338cdbe35c37704b
Ignacio Nieto <66888010+Daniel-Ignacio-Nieto@users.noreply.github.com>
2021-01-24 16:29:37 -0300

    Revert "Student pc"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from "components/Login/Login";
 import Nav from "components/Nav/Nav";
-import PanelControl from 'components/PanelControlStudents/panelControl';
+import Invitacion from 'components/InvitacionAlumnos/Invitacion';
 import Home from 'components/Home/Home';
-
 
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
     <>
       <Nav />
       <Route exact path='/' component={Login} />
-      <Route exact path='/PanelControlStudent' component={PanelControl} />
+      <Route exact path='/invitation' component={Invitacion} />
       <Route exact path='/Home' component={Home} />
     </>
   );
