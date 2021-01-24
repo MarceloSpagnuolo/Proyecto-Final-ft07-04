892d8993da3e30d0706494fb8c51312c06c6d5a0
MarceloSpagnuolo <71037386+MarceloSpagnuolo@users.noreply.github.com>
2021-01-24 20:18:21 -0300

    Gestion de cohortes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Login from "components/Login/Login";
 import Nav from "components/Nav/Nav";
+import Cohortes from "components/Cohortes/Cohortes";
+import Activos from "components/Cohortes/Activos";
 
 function App() {
   return (
     <>
       <Nav />
       <Route exact path="/" component={Login} />
+      <Route exact path="/cohortes" component={Cohortes} />
+      <Route exact path="/activos/:id" component={Activos} />
     </>
   );
 }
---
+++ client/src/components/Cohortes/Add.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Formik, Field, ErrorMessage, Form } from "formik";
+import "./Add.css";
+
+interface AddForm {
+  nroCohorte: Number;
+  fechaInicio: Date;
+}
+
+function Add() {
+  const initialValues: AddForm = {
+    nroCohorte: 0,
+    fechaInicio: new Date(),
+  };
+
+  return (
+    <>
+      <div className="Add-Body">
+        <div className="Add-Form-Body">
+          <Formik
+            initialValues={initialValues}
+            validate={(values) => {
+              var errors: { [k: string]: any } = {};
+              if (values.nroCohorte < 1)
+                errors.nroCohorte = "Debe Ingresar Nro de Cohorte";
+              if (values.fechaInicio)
+                errors.fechaInicio = "Debe Ingresar una fecha válida";
+
+              return errors;
+            }}
+            onSubmit={(values) => {}}
+          >
+            {({ isSubmitting, isValid }) => (
+              <>
+                <div className="Add-Cohorte-Body">
+                  <h2 className="Add-Cohorte-Title">Nuevo Cohorte</h2>
+                  <p className="Add-Frase">
+                    Acá iba a poner una frase super importante y reboluda pero
+                    no se me ocurrió nada. Asi que solamente voy a decir gracias
+                    por ponerse a leer esta pelotudez
+                  </p>
+                  <Form className="Add-Form">
+                    <div className="Add-Form-Campos">
+                      <label htmlFor="Add" className="Add-Form-Label">
+                        Nro. Cohorte*
+                      </label>
+                      <br />
+                      <Field
+                        className="Add-Form-Input-Cohorte"
+                        type="number"
+                        name="nroCohorte"
+                      />
+                      <ErrorMessage name="nroCohorte">
+                        {(message) => (
+                          <div className="Add-Form-Error">{message}</div>
+                        )}
+                      </ErrorMessage>
+                    </div>
+                    <div className="Add-Form-Campos">
+                      <label htmlFor="Add" className="Add-From-Label">
+                        Fecha de Inicio*
+                      </label>
+                      <br />
+                      <Field
+                        className="Add-Form-Input-Fecha"
+                        type="date"
+                        name="fechaInicio"
+                      />
+                      <ErrorMessage name="fechaInicio">
+                        {(message) => (
+                          <div className="Add-Form-Error">{message}</div>
+                        )}
+                      </ErrorMessage>
+                    </div>
+                    <span>* Campos obligatorios</span>
+                    <br />
+                    <button
+                      type="submit"
+                      className={`submit ${
+                        isSubmitting || !isValid ? "disabled" : "enabled"
+                      }`}
+                      id="Add-Btn-Submit"
+                      disabled={isSubmitting || !isValid}
+                    >
+                      Generar Cohorte
+                    </button>
+                  </Form>
+                </div>
+              </>
+            )}
+          </Formik>
+        </div>
+        <div className="Add-Image">
+          <img
+            src="https://cdn.discordapp.com/attachments/801584401611620383/802716856850972683/undraw_group_chat_v059.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Add;
