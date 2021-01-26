97826f3c927c6688358067ed1837be42ef05c3ca
Daniel-Ignacio-Nieto <ignacionieto65@hotmail.com>
2021-01-26 01:31:14 -0300

    progreso de ruta mailgun

--- api/src/Routes/mailgun.ts
+++ api/src/Routes/mailgun.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import Registro from "../MailModel/Registro"
 const router = express.Router();
+const XLSX = require("xlsx");
+
 
 
 router.post('/', (req,res) => {
-    const {email} = req.body;
+    const {file, email} = req.body;
+
+    const excelToJSON = () => {
+        const excel = XLSX.readFile(file); //lee el archivo
+        var hojas = excel.SheetNames; //devuelve un array con el contenido de cada hoja [{}, {0bj2}, {obj3}]
+
+        let datos = XLSX.utils.sheet_to_json(excel.Sheets[hojas[0]]);
+
+
+    }
     Registro(email)
     
     res.sendStatus(200)
