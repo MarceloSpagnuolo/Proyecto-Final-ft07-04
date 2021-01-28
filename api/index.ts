61f033f75f4f1aabc6f1e9053eacd2b735a2f1ba
MarceloSpagnuolo <71037386+MarceloSpagnuolo@users.noreply.github.com>
2021-01-28 12:44:54 -0300

    mejoras en el insert y modelos

--- api/index.ts
+++ api/index.ts
@@ -2,10 +2,9 @@ import server from "./src/app";
 import connectDB from "./src/db";
 import Insert from "./insert";
 
-
 server.listen(3001, () => {
   //Genera la conexión en el puerto establecida en db.ts
   // Se le pasa un parámetro como true o false. Si es true, borra la DB e inserta una nueva.
   // Si es false, no hace nada, sólo se conecta.
-  connectDB(true);
+  connectDB(false);
 });
