39197490c58c244f903abd5e415407edea3b3010
carlirows <carlirows@gmail.com>
2021-01-27 21:43:25 -0500

    algunos estilos aplicados a la version desktop del home

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import alumnos from '../../assets/Alumnos.png';
+import alumnos from '../../assets/boton-estudiantes.jpg';
 import cohortes from '../../assets/Cohortes.png';
 import topImg from '../../assets/topBanner.png';
 import PanelControlStudents from 'components/PanelControlStudents/panelControl';
@@ -12,21 +12,28 @@ const Home = (): JSX.Element => {
     return (
         <div id="super">
             <img id="img-sup" src={topImg} alt="banner superior" />
-            <h1>Panel de Control General</h1>
-            <h2>Gestiona alumnos y cohortes</h2>
+            <h1>Bienvenido al panel del administrador</h1>
+
             <div id="bar-yellow"></div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore error aspernatur est consequuntur aliquam obcaecati fugiat necessitatibus, sint magnam accusantium, dolorem atque illo facere quisquam voluptatibus, modi ipsa architecto! Voluptates!</p>
+
             <br />
-            <div className="container-home">
-                <Link rel="stylesheet" to="/PanelControlStudent">
-                    <img src={alumnos} alt="Redirect Alumnos" onClick={() => PanelControlStudents} />
-                </Link>
+            <div id="container-home">
+                <div id='container-home-estudiantes'>
+                    <Link rel="stylesheet" to="/PanelControlStudent">
+                        <img id='home-estudiantes-boton' src={alumnos} alt="Redirect Alumnos" onClick={() => PanelControlStudents} />
+                    </Link>
+                    <div>
+                        <div id='alumnos-content'><h1>Alumnos</h1></div>
+                        <div id='alumnos-title-home'></div>
+                    </div>
+                </div>
+                <br></br>
                 <Link rel="stylesheet" to="/Cohortes">
-                    <img src={cohortes} alt="Redirect Cohortes" onClick={() => Cohortes} />
+                    <img id='home-estudiantes-boton' src={cohortes} alt="Redirect Cohortes" onClick={() => Cohortes} />
                 </Link>
             </div>
         </div>
     )
 }
 
-export default Home;
+export default Home
