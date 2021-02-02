f26eda256cd341bcfe483fcea49b004bc378fa0e
Carla <carlaellyana1107@gmail.com>
2021-02-02 17:25:12 -0500

    avance en tabla vista mobile

--- client/src/Store/Reducer/Standups.tsx
+++ client/src/Store/Reducer/Standups.tsx
@@ -1,7 +1,6 @@
 import Swal from "sweetalert2";
 import {
   GET_STANDUPS,
-  PUT_STANDUPS,
   POST_PM,
   GET_ONE_STANDUP,
   POST_STANDUP,
@@ -31,8 +30,8 @@ function Standups(state = inicialState, action: any) {
         standup: action.payload
       }
     }
-    case POST_PM:{
-      return{
+    case POST_PM: {
+      return {
         standup: action.payload
       }
     }
@@ -49,15 +48,13 @@ function Standups(state = inicialState, action: any) {
       }
     }
     case ERROR_MESSAGE: {
-      alert(action.message);
-    case ERROR_MESSAGE: {
       Swal.fire(
         "Error",
         action.message,
         "error"
       );
       return state;
-      }
+    }
     default: {
       return state;
     }
